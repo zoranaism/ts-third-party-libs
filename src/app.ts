7325dfe172ed6e61e6652bc7fa983cfee1801d0f
zoranaism <lazareviczorana@gmail.com>
2020-07-29 10:43:37 +0200

    class-transformer, class-validator, declare and Last Resort

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,40 @@
-import _ from 'lodash';
+import "reflect-metadata";
+import { plainToClass } from "class-transformer"; // class tranformer
+import { validate } from "class-validator";
+
+import _ from "lodash"; // lodash
+import { Product } from "./product.model";
 
 console.log(_.shuffle([1, 2, 3]));
+
+declare var GLOBAL: any;
+
+console.log(GLOBAL);
+
+const products = [
+  { title: "A Carpet", price: 29.99 },
+  { title: "A Book", price: 10.99 },
+];
+
+const newProd = new Product("", -5.99);
+validate(newProd).then((errors) => {
+  if (errors.length > 0) {
+    console.log("Validation errors!");
+    console.log(errors);
+  } else {
+    console.log(newProd.getInformation());
+  }
+});
+
+// const p1 = new Product("A Book", 12.99);
+
+// const loadedProducts = products.map((prod) => {
+//   return new Product(prod.title, prod.price);
+// });
+
+// THIS WILL TRANFORM ANY OBJECT TO AN INSTANCE OF THE Product CLASS
+const loadedProducts = plainToClass(Product, products);
+
+for (const prod of loadedProducts) {
+  console.log(prod.getInformation());
+}
